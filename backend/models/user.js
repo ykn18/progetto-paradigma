354c1b2e14fa1f6f5850c0310cb07af775bb218a
Gianluca Arena <arena.gianluca@outlook.it>
2019-11-28 11:49:35 +0100

    Fix like and add favorite

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 
-    favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
 });
 
 const User = mongoose.model('User', userSchema);
